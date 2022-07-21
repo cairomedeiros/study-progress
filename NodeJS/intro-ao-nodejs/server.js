b19b8a81e18f3b2a738780dab59951e78d044b63
cairomedeiros <cairomedeiros13@gmail.com>
2022-07-21 11:24:46 -0300

    server + ramUsage

--- NodeJS/intro-ao-nodejs/server.js
+++ NodeJS/intro-ao-nodejs/server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 const host = 'http://localhost';
 const port = 3000;
+const stats = require('./pcRamUsage.js');
 
 http.createServer((req, res) => {
-    res.end('<h1>working</h1>')
+    let url = req.url;
+
+    if(url === '/stats'){
+        res.end(JSON.stringify(stats, null, 2));
+    }else{
+        res.end('<h1>bien venidos</h1>')
+    }
+    
 } ).listen(port, () => console.log(`server is running on ${host}:${port}`))
