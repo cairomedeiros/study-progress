cbb0d5e9335debfd59719c7b01c1ee3bae08e856
cairomedeiros <cairomedeiros13@gmail.com>
2022-07-28 17:12:37 -0300

    crud

--- NodeJS/nodejs-zero-a-maestria/node-mysql/instalacao-mysql/index.js
+++ NodeJS/nodejs-zero-a-maestria/node-mysql/instalacao-mysql/index.js
@@ -68,6 +68,54 @@ app.get('/books/:id', function (req, res) {
   
       res.render('book', { book })
     })
+  });
+
+  app.get('/books/edit/:id', function (req, res) {
+    const id = req.params.id
+  
+    const query = `SELECT * FROM books WHERE id = ${id}`
+  
+    conn.query(query, function (err, data) {
+      if (err) {
+        console.log(err)
+      }
+  
+      const book = data[0]
+  
+      console.log(data[0])
+  
+      res.render('editbook', { book })
+    })
+  });
+
+  app.post('/books/updatebook', (req, res) => {
+    const id = req.body.id
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+  
+    const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`
+  
+    conn.query(sql, function (err) {
+      if (err) {
+        console.log(err)
+      }
+  
+      res.redirect(`/books`)
+    })
+  });
+
+  app.post('/books/remove/:id', function (req, res) {
+    const id = req.params.id
+  
+    const query = `DELETE FROM books WHERE id = ${id}`
+  
+    conn.query(query, function (err) {
+      if (err) {
+        console.log(err)
+      }
+  
+      res.redirect(`/books`)
+    })
   })
 
 const conn = mysql.createConnection({
