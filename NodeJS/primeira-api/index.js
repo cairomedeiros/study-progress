3135a97ab6ae6e9be6f7678b54d2e0dc9fdfff40
cairomedeiros <cairomedeiros13@gmail.com>
2022-07-29 09:25:53 -0300

    first api node

---
+++ NodeJS/primeira-api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json())
+
+//rotas - endpoints
+app.get('/', (req, res) => {
+    res.json({message: 'Primeira rota criada com sucesso'})
+});
+
+app.listen(3000);
