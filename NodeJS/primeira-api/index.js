148dfbf89530f23ea71d3bb27d7d55de52c09dc4
cairomedeiros <cairomedeiros13@gmail.com>
2022-07-29 10:50:49 -0300

    errors

--- NodeJS/primeira-api/index.js
+++ NodeJS/primeira-api/index.js
@@ -14,15 +14,19 @@ app.use(express.json())
 app.post('/createproduct', (req, res) => {
     const name = req.body.name
     const price = req.body.price
+
+    if(!name){
+        res.status(422).json({message: 'o campo name não foi preenchido!'})
+    }
   
     console.log(name)
     console.log(price)
   
-    res.json({ message: `O produto: ${name} foi criado com sucesso!` })
+    res.status(201).json({ message: `O produto: ${name} foi criado com sucesso!` })
   });
 
 app.get('/', (req, res) => {
-    res.json({message: 'Primeira rota criada com sucesso'})
+    res.status(200).json({message: 'Primeira rota criada com sucesso'})
 });
 
 app.listen(3000);
