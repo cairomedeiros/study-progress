f2d668e6fa35d803c390c94eb29d57b832216c16
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-02 17:07:37 -0300

    switch

---
+++ Java/EstruturaCondicionalSwitch/app/src/main/java/EstruturaCondicionalSwitch/App.java
@@ -0,0 +1,27 @@
+
+package EstruturaCondicionalSwitch;
+
+public class App {
+    
+
+    public static void main(String[] args) {
+        
+        int codigoProduto = 1;
+        
+        switch(codigoProduto){
+            case 1: System.out.println("hello");
+                
+                break;
+            case 2:
+                
+                break;
+                
+            case 3:
+                
+                break;
+                
+            default:
+                break;
+        }
+    }
+}
