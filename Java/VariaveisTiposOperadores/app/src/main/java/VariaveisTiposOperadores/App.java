e6aa408e7dce52f8e057162ef05ef1a04b82e351
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-02 16:36:24 -0300

    variaveis

---
+++ Java/VariaveisTiposOperadores/app/src/main/java/VariaveisTiposOperadores/App.java
@@ -0,0 +1,18 @@
+
+package VariaveisTiposOperadores;
+
+public class App {
+    
+
+    public static void main(String[] args) {
+        int idade = 10;
+        float cotacaodolar = 5.0f;
+        double cotacaoeuro = 5.0d;
+        char genero = 'M';
+        byte ponto = 0;
+        boolean cadastrado = false;
+        
+        
+        System.out.print(genero);
+    }
+}
