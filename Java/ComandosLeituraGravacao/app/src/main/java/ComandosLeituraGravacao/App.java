c1f4ea2cb03e86e73773ce3fd966a8e12a91ee3d
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-02 15:52:00 -0300

    .

---
+++ Java/ComandosLeituraGravacao/app/src/main/java/ComandosLeituraGravacao/App.java
@@ -0,0 +1,17 @@
+package ComandosLeituraGravacao;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        int idade = leitor.nextInt(10);
+        
+        System.out.println(idade);
+        
+           
+    }
+}
