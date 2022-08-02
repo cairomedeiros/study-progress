0b4de4ff154e731237b57c5176d43dcfddbe9874
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-02 16:58:36 -0300

    if

---
+++ Java/EstruturaCondicionalIf/app/src/main/java/EstruturaCondicionalIf/App.java
@@ -0,0 +1,20 @@
+package EstruturaCondicionalIf;
+
+public class App {
+    
+
+    public static void main(String[] args) {
+        
+        int media = 10;
+        
+       if(media >=7){
+           if(media == 10){
+               System.out.println("PARABÉNS");
+           }else{
+               System.out.println("Aprovado");
+           }
+       }else{
+           System.out.println("reprovado amigo");
+       }
+    }
+}
