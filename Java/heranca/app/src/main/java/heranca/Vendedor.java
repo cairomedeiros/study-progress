0109e48f81c6d458adb34359ad953fc37c3a58ad
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-03 18:59:17 -0300

    heranca

---
+++ Java/heranca/app/src/main/java/heranca/Vendedor.java
@@ -0,0 +1,11 @@
+package heranca;
+
+public class Vendedor extends Funcionario{
+    
+    private int totalItensVendidos;
+    private float comissaoPorItem;
+    
+    public Vendedor(){
+        super();
+}
+}
