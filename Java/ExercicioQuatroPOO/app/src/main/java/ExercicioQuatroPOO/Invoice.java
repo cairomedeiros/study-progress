f56bde782b6caf9a73f02c0ec909b58a038285fb
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-06 20:07:00 -0300

    getters and setters

--- Java/ExercicioQuatroPOO/app/src/main/java/ExercicioQuatroPOO/Invoice.java
+++ Java/ExercicioQuatroPOO/app/src/main/java/ExercicioQuatroPOO/Invoice.java
@@ -10,9 +10,54 @@ public class Invoice {
     public Invoice(int item, String descricao, int quantComprada, float precoUnit) {
         this.item = item;
         this.descricao = descricao;
+        
+        if(quantComprada < 0){
+            this.quantComprada = 0;
+        }else{
+            this.quantComprada = quantComprada;
+        }
+        
+        if(precoUnit < 0){
+            this.precoUnit = 0;
+        }else{
+            this.precoUnit = precoUnit;
+        }
+        
+    }
+
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantComprada() {
+        return quantComprada;
+    }
+
+    public void setQuantComprada(int quantComprada) {
         this.quantComprada = quantComprada;
+    }
+
+    public float getPrecoUnit() {
+        return precoUnit;
+    }
+
+    public void setPrecoUnit(float precoUnit) {
         this.precoUnit = precoUnit;
     }
+    
+    
 
     
     
