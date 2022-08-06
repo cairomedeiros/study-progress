364975c96b113137b4332d4deb8261b1583abebd
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-06 20:03:44 -0300

    constructor

--- Java/ExercicioQuatroPOO/app/src/main/java/ExercicioQuatroPOO/Invoice.java
+++ Java/ExercicioQuatroPOO/app/src/main/java/ExercicioQuatroPOO/Invoice.java
@@ -6,4 +6,15 @@ public class Invoice {
     private String descricao;
     private int quantComprada;
     private float precoUnit;
+
+    public Invoice(int item, String descricao, int quantComprada, float precoUnit) {
+        this.item = item;
+        this.descricao = descricao;
+        this.quantComprada = quantComprada;
+        this.precoUnit = precoUnit;
+    }
+
+    
+    
+    
 }
