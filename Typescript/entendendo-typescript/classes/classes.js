e4fb27ebf79f421ab4ed40d140edcab9f16b4b20
cairomedeiros <cairomedeiros13@gmail.com>
2022-08-23 15:30:49 -0300

    .

--- Typescript/entendendo-typescript/classes/classes.js
+++ Typescript/entendendo-typescript/classes/classes.js
@@ -6,7 +6,14 @@ class Produto {
         this.preco = preco;
         this.desconto = desconto;
     }
+    resumo() {
+        return `${this.nome} custa R$${this.precoComDesconto()} (${this.desconto * 100}% off)`;
+    }
+    precoComDesconto() {
+        return this.preco * (1 - this.desconto);
+    }
 }
-const cadeira = new Produto("cadeira", 1300, 0);
+const cadeira = new Produto("cadeira", 1300, 0.09);
+console.log(cadeira.resumo());
 const teclado = new Produto("Teaclado Red Dragon", 445.99);
 //# sourceMappingURL=classes.js.map
--- Typescript/entendendo-typescript/classes/classes.ts
+++ Typescript/entendendo-typescript/classes/classes.ts
@@ -4,9 +4,18 @@ class Produto {
     constructor(public nome: string, public preco: number, public desconto: number = 0 ){
         
     }
+
+    public resumo(): string {
+        return `${this.nome} custa R$${this.precoComDesconto()} (${this.desconto * 100}% off)`
+    }
+
+    public precoComDesconto(): number {
+         return this.preco * (1 - this.desconto);
+    }
 }
 
-const cadeira = new Produto("cadeira", 1300, 0);
+const cadeira = new Produto("cadeira", 1300, 0.09);
+console.log(cadeira.resumo())
 
 const teclado = new Produto("Teaclado Red Dragon", 445.99)
 
