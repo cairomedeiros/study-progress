8c1a072a8e3a3215bf58998cf0dc5706b8ccea59
cairomedeiros <cairomedeiros13@gmail.com>
2022-09-13 08:39:49 -0300

    list

--- Java/devsuperior-java/exerciciolist/src/principal/program.java
+++ Java/devsuperior-java/exerciciolist/src/principal/program.java
@@ -37,11 +37,35 @@ public class program {
 			
 		}
 		
+		System.out.print("enter the employee id that will have salary increase: ");
+		int idsalary = sc.nextInt();
+		Integer pos = position(list, idsalary);
+		if(pos == null) {
+			System.out.println("id does not exist!");
+		}else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
 		
-		
+		System.out.println();
+		System.out.println("List of employees");
+		for(Employee emp : list) {
+			System.out.println(emp);
+		}
 		
 		sc.close();
 
+	}
+	
+	public static Integer position(List<Employee> list, int id) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getId() == id) {
+				return i;
+			}
+				
+		}
+		return null;
 	}
 
 }
