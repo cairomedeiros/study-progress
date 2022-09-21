e047d71f73d6616e31258c014cfd2fe1956e0a7c
cairomedeiros <cairomedeiros13@gmail.com>
2022-09-21 10:22:55 -0300

    try and catch

---
+++ Java/exercicios-java/tryAndCatch/src/model/exception/Exceptions.java
@@ -0,0 +1,14 @@
+package model.exception;
+
+public class Exceptions extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	public Exceptions(String msg) {
+		super(msg);
+	}
+	
+	
+	
+	
+	
+}
