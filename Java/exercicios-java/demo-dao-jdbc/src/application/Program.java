e91a73e2dc1c3c6e9a7923d77375b480c5e39719
cairomedeiros <cairomedeiros13@gmail.com>
2022-09-23 14:59:01 -0300

    seller entitie class

--- Java/exercicios-java/demo-dao-jdbc/src/application/Program.java
+++ Java/exercicios-java/demo-dao-jdbc/src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
-import model.entities.Departament;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departament obj = new Departament(1, "Books");
-		System.out.println(obj);
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(22, "Cairo", "cairo@gmail.com", new Date(), 2500.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
