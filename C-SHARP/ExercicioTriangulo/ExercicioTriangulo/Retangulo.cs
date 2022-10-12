97dec942cecbc85f2f45358671ca34624b49603a
cairomedeiros <cairomedeiros13@gmail.com>
2022-10-12 14:48:47 -0300

    exercicio retangulo

---
+++ C-SHARP/ExercicioTriangulo/ExercicioTriangulo/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTriangulo {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+
+        override public string ToString() {
+            return "AREA: "
+                + Area()
+                + " PERÍMETRO: "
+                + Perimetro()
+                + " DIAGONAL: "
+                + Diagonal();
+        }
+    }
+}
