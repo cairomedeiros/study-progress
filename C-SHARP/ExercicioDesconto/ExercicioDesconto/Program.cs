b1d78d525953dd46ce96b7fb1e25425278b0aa07
cairomedeiros <cairomedeiros13@gmail.com>
2022-10-12 16:10:50 -0300

    alunos

---
+++ C-SHARP/ExercicioDesconto/ExercicioDesconto/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExercicioDesconto {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Nome: ");
+            aluno.Name = Console.ReadLine();
+            Console.WriteLine("Primeiro trimestre: ");
+            aluno.NotaUm = double.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo trimestre: ");
+            aluno.NotaDois = double.Parse(Console.ReadLine());
+            Console.WriteLine("Terceiro trimestre: ");
+            aluno.NotaTres = double.Parse(Console.ReadLine());
+
+            if(aluno.NotaFinal() >= 60) {
+                Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal());
+                Console.WriteLine("APROVADO");
+            }else if (aluno.NotaFinal() < 60) {
+                Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal());
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60 - aluno.NotaFinal()) + " PONTOS");
+            }
+            else {
+                Console.WriteLine("algo deu errado");
+            }
+
+           
+
+        }
+    }
+}
