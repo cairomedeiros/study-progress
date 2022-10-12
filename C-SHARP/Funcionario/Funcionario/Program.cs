94b7b382b3d8c5e1c5b90c766c21b942ed731ddb
cairomedeiros <cairomedeiros13@gmail.com>
2022-10-12 15:34:58 -0300

    exercicio funcionarios

---
+++ C-SHARP/Funcionario/Funcionario/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Funcionario {
+    internal class Program {
+        static void Main(string[] args) {
+            FuncionarioEntitie f = new FuncionarioEntitie();
+
+            Console.WriteLine("Nome:");
+            f.Name = Console.ReadLine();
+            Console.WriteLine("Salário Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionário: " + f.Name + ", $ " + f.SalarioLiquido());
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+            f.AumentarSalario(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + f.Name + ", $ " + f.SalarioLiquido());
+
+
+        }
+    }
+}
