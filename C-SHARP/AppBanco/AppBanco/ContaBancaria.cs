3786306fdd004c8e487d57a40eb547933bd7cd51
cairomedeiros <cairomedeiros13@gmail.com>
2022-10-19 12:36:38 -0300

    first step exercise account

---
+++ C-SHARP/AppBanco/AppBanco/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBanco
+{
+    internal class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Saldo = saldo;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo = Saldo - quantia - 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " 
+                + Numero 
+                + ", Titular: "
+                + Titular 
+                + ", Saldo: " 
+                +Saldo;
+        }
+
+    }
+}
---
+++ C-SHARP/AppBanco/AppBanco/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AppBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c = new ContaBancaria();
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial? ");
+            string confirm = Console.ReadLine();
+            string confirmUpper = confirm.ToUpper();
+            char first = confirmUpper[0];
+
+            if(first == 'S')
+            {
+                Console.Write("Entre com o valor de deposito inicial: ");
+                double saldo = double.Parse(Console.ReadLine());
+
+                ContaBancaria cc = new ContaBancaria(numero, titular, saldo);
+
+                Console.WriteLine(cc);
+            }
+            else if(first == 'N')
+            {
+                Console.WriteLine("salut!");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+           
+
+           
+        }
+    }
+}
