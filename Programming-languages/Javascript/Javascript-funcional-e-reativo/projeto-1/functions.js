8bf4fb410841a6c90ab677e898f61048b03441eb
cairomedeiros <cairomedeiros13@gmail.com>
2023-03-14 21:45:31 -0300

    refatorando função de ler arquivos

--- Programming-languages/Javascript/Javascript-funcional-e-reativo/projeto-1/functions.js
+++ Programming-languages/Javascript/Javascript-funcional-e-reativo/projeto-1/functions.js
@@ -17,22 +17,25 @@ function elementosTerminadosCom(array, padrao){
     return array.filter(el => el.endsWith(padrao))
 }
 
-function lerConteudoArquivos(arquivosLidos){
-    let all = '';
-    arquivosLidos.map(item => {
-        fs.readFile(item, 'utf8', (err, data) => {
-            if(!undefined){
-                console.log(all)
-                all = all + data
-            }
-        })
+function lerArquivo(caminho){
+    return new Promise((resolve, reject) => {
+        try{
+            const conteudo = fs.readFileSync(caminho, {encoding: 'utf-8'})
+            resolve(conteudo.toString())
+        } catch(e){
+            reject(e)
+        }
+        
     })
+}
 
-    return all;
+function lerArquivos(caminhos){
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
 module.exports = {
     lerDiretorio,
-    elementosTerminadosCom,
-    lerConteudoArquivos
+    lerArquivo,
+    lerArquivos,
+    elementosTerminadosCom
 }
--- Programming-languages/Javascript/Javascript-funcional-e-reativo/projeto-1/main.js
+++ Programming-languages/Javascript/Javascript-funcional-e-reativo/projeto-1/main.js
@@ -5,6 +5,5 @@ const caminho = path.join(__dirname, 'legendas')
 
 fn.lerDiretorio(caminho)
     .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-    .then(arquivosLidos => fn.lerConteudoArquivos(arquivosLidos))
-    .then(todoConteudo => todoConteudo.split('\n'))
-    .then(t => console.log(t))
+    .then(arquivosLidos => fn.lerArquivos(arquivosLidos))
+    .then(console.log)
