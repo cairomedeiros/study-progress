5ee60e965acd49d3a2c5a55a579cac169dde03fa
cairomedeiros <cairomedeiros13@gmail.com>
2023-09-21 21:30:33 -0300

    adição de classe, construtor, getters and setters, hashCode, equals e toString

--- Java/spring-boot-example/src/main/java/com/cairo/Main.java
+++ Java/spring-boot-example/src/main/java/com/cairo/Main.java
@@ -2,33 +2,89 @@ package com.cairo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
+
     }
 
-    @GetMapping("/home")
-    public GreetResponse greet(){
-        GreetResponse response = new GreetResponse(
-                "Hello",
-                List.of("Java", "Golang", "Rust"),
-                new Person("Alex", 1, 20.0)
-        );
-        return response;
-    }
+    class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
 
-    record Person(String name, int age, double savings){}
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ){}
+        public Customer(){};
+
+        public Customer(Integer id, String name, String email, Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
 
 }
